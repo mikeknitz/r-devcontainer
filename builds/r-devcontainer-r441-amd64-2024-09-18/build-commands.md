e4d63fd9bc170fe35cb8b70ddb77a7e697d84f99
mikeknitz <mikeknitz@gmail.com>
2024-09-18 15:29:27 -0600

    built container knitz/r-devcontainer:r441-amd64-2024-09-18

---
+++ builds/r-devcontainer-r441-amd64-2024-09-18/build-commands.md
@@ -0,0 +1,45 @@
+## Commands to create and push image
+
+Point working directory to folder where `Dockerfile` and `build-scripts/` exist for this particular build:
+
+``` bash
+cd builds/r-devcontainer-r441-amd64-2024-09-18
+```
+
+Populate `build-scripts/` directory with subdirectories containing installation code for devcontainer features
+
+``` bash
+bash build-scripts/pull-build-scripts.sh
+```
+
+Build Docker image
+
+``` bash
+docker build -t <docker-hub-username>/r-devcontainer:r441-amd64-2024-09-18 .
+```
+
+Push to Docker Hub
+
+``` bash
+docker push <docker-hub-username>/r-devcontainer:r441-amd64-2024-09-18
+```
+
+## Summary of build
+
+- Pull base Ubuntu image with R built in
+- Add non-root user "ubuntu"
+- Copy `build-scripts/` to `/tmp/build-scripts/` inside the image
+- Run devcontainer feature scripts as the root user
+- Copy in files and configs:
+  - Helper scripts `start-rstudio` and `copy-code-workspace`
+  - `.lintr`, `.Rprofile`, `.zshrc`, `rstudio-prefs.json`
+- Install:
+  - TinyTeX (through <https://yihui.org/tinytex/>)
+  - Chromium (through `quarto install`)
+- Run `container-setup.sh`:
+  - Install system dependencies and software with `apt-get`
+  - Install radian terminal [GitHub - randy3k/radian: A 21 century R console](https://github.com/randy3k/radian)
+  - Install and configure fonts with `ttf-mscorefonts-installer` <https://packages.debian.org/sid/fonts/ttf-mscorefonts-installer>
+  - Install CRAN and Bioconductor packages (see `build-scripts/container-setup.sh`)
+  - Import fonts to R with `extrafont` package: [CRAN: Package extrafont](https://cran.r-project.org/web/packages/extrafont/index.html)
+- End with `CMD ["/usr/bin/zsh"]` (configured as the non-root users's default shell)
---
+++ builds/r-devcontainer-r441-amd64-2024-09-18/build-scripts/container-setup.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+set -e
+
+# System dependencies
+sudo apt-get update
+
+# Python / pip / radian
+sudo apt-get install -y python3-pip
+sudo pip3 install -U radian
+
+# Git
+git config --global init.defaultBranch main
+git config --global pull.ff only
+git config --global pager.branch false
+
+# tidyverse, devtools, languageserver compilation
+sudo apt-get install -y \
+  libcurl4-openssl-dev  \
+  libssl-dev            \
+  libxml2-dev           \
+  libfontconfig1-dev    \
+  libharfbuzz-dev       \
+  libfribidi-dev        \
+  libfreetype6-dev      \
+  libpng-dev            \
+  libtiff5-dev          \
+  libjpeg-dev           \
+  libgit2-dev
+
+# Fonts
+# https://askubuntu.com/questions/651441/how-to-install-arial-font-and-other-windows-fonts-in-ubuntu
+echo "ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true" | sudo debconf-set-selections
+sudo apt-get install -y ttf-mscorefonts-installer
+sudo fc-cache -f
+
+# httpgd compilation
+sudo apt-get install -y libcairo2-dev
+
+# Seurat compilation
+sudo apt-get install -y libglpk-dev
+
+# HDF5 for h5 single cell objects and loupeR package
+sudo apt-get install -y libhdf5-dev
+
+# Magick, helps for R magick and ggkegg packages
+sudo apt-get install -y imagemagick     
+sudo apt-get install -y libmagick++-dev 
+
+# R packages
+
+R -q -e 'install.packages("tidyverse")'
+R -q -e 'install.packages("devtools")'
+R -q -e 'install.packages("languageserver")'
+R -q -e 'devtools::install_github("nx10/httpgd@b0a5ec38638ca69e3adbc7b24d815757e5f74817")' # 2024-09-18
+R -q -e 'install.packages("extrafont")'
+R -q -e 'install.packages("Cairo")'
+R -q -e 'install.packages("BiocManager")'
+R -q -e 'install.packages("hexbin")' # Recommended for vsn or impute I think
+
+R -q -e 'BiocManager::install(version = "3.19",       update = TRUE, ask = FALSE, checkBuilt = TRUE)'
+R -q -e 'BiocManager::install("SummarizedExperiment", update = TRUE, ask = FALSE, checkBuilt = TRUE)'
+R -q -e 'BiocManager::install("AnnotationDbi",        update = TRUE, ask = FALSE, checkBuilt = TRUE)'
+R -q -e 'BiocManager::install("org.Hs.eg.db",         update = TRUE, ask = FALSE, checkBuilt = TRUE)'
+R -q -e 'BiocManager::install("org.Mm.eg.db",         update = TRUE, ask = FALSE, checkBuilt = TRUE)'
+R -q -e 'BiocManager::install("DESeq2",               update = TRUE, ask = FALSE, checkBuilt = TRUE)'
+R -q -e 'BiocManager::install("clusterProfiler",      update = TRUE, ask = FALSE, checkBuilt = TRUE)'
+R -q -e 'BiocManager::install("ComplexHeatmap",       update = TRUE, ask = FALSE, checkBuilt = TRUE)'
+R -q -e 'BiocManager::install("limma",                update = TRUE, ask = FALSE, checkBuilt = TRUE)'
+R -q -e 'BiocManager::install("PhosR",                update = TRUE, ask = FALSE, checkBuilt = TRUE)'
+R -q -e 'BiocManager::install("vsn",                  update = TRUE, ask = FALSE, checkBuilt = TRUE)'
+R -q -e 'BiocManager::install("impute",               update = TRUE, ask = FALSE, checkBuilt = TRUE)'
+
+# Import fonts
+R -q -e 'extrafont::font_import(prompt = FALSE); extrafont::loadfonts()'
+
+R -q -e 'install.packages("imputeLCMD")' # Relies on impute I think
+R -q -e 'install.packages("Hmisc")'
+R -q -e 'install.packages("magick", repos = "https://packagemanager.posit.co/cran/2024-09-18", type = "source")'
+R -q -e 'BiocManager::install("ggkegg", update = TRUE, ask = FALSE, checkBuilt = TRUE)'
+
+R -q -e 'install.packages("Seurat")'
+R -q -e 'devtools::install_github("immunogenomics/presto@7636b3d0465c468c35853f82f1717d3a64b3c8f6")' # 2024-09-18
+R -q -e 'install.packages("clustree")'
+R -q -e 'install.packages("DT")'
+R -q -e 'install.packages("ggh4x")'
+R -q -e 'install.packages("ggsci")'
+R -q -e 'install.packages("ggpubr")'
+R -q -e 'install.packages("ggplotify")'
+R -q -e 'install.packages("HGNChelper")'
+R -q -e 'install.packages("openxlsx")'
+R -q -e 'install.packages("ggnewscale")'
+R -q -e 'install.packages("ggbeeswarm")'
+R -q -e 'install.packages("dendsort")'
+R -q -e 'install.packages("msigdbr")'
+R -q -e 'install.packages("hdf5r")'
+
+# Compile Seurat and SeuratObject from source to avoid
+  # "built under a different version" warning when loading
+# Currently no binaries for arm builds from Posit so this part
+  # doesn't matter for arm architecture
+R -q -e 'remove.packages("Seurat")'
+R -q -e 'install.packages("Seurat", repos = "https://packagemanager.posit.co/cran/2024-09-18", type = "source")'
+R -q -e 'remove.packages("SeuratObject")'
+R -q -e 'install.packages("SeuratObject", repos = "https://packagemanager.posit.co/cran/2024-09-18", type = "source")'
+
+# Clean up
+# https://askubuntu.com/questions/3167/what-is-difference-between-the-options-autoclean-autoremove-and-clean
+# https://stackoverflow.com/questions/33902669/good-practice-at-end-of-dockerfile
+sudo apt-get autoremove -y
+sudo apt-get clean -y
+sudo rm -rf /var/lib/apt/lists/*
